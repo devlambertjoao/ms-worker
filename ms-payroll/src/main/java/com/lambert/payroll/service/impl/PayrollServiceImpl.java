a22549ce6aa11e9a3a316193aa163c91fc9034f1
Joao Lambert <devlambertjoao@gmail.com>
2021-07-13 09:41:58 -0300

    Fixing file

--- ms-payroll/src/main/java/com/lambert/payroll/service/impl/PayrollServiceImpl.java
+++ ms-payroll/src/main/java/com/lambert/payroll/service/impl/PayrollServiceImpl.java
@@ -1,8 +1,5 @@
 package com.lambert.payroll.service.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
